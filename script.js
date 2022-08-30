cb13d56c26ea1017b79d399c10879058b19592c2
TeePee221 <teeepeeee1@gmail.com>
2022-08-30 11:40:02 +0200

    Main funciton working. One round played properly

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 const choices = ["rock", "paper", "scissors"]
+const playerChoice = prompt("Choose").toLowerCase();
+console.log(`Player has chosen ${playerChoice}!!!`)
+
+
+
 function computerChoice () {
     const choice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(choice)
+    console.log(`Computer has chosen ${choice}!!`);
+    return choice
+    
 }
-computerChoice();
+
+function checkWinner (playerChoice, choice){
+    if(playerChoice == computerChoice){
+        return "tie";
+    }
+    else if( (playerChoice == "rock" && computerChoice == "scissors") ||
+            (playerChoice == "scissors" && computerChoice == "paper") ||
+            (playerChoice == "paper" && computerChoice == "rock")
+            ) {
+                return "player"
+            }
+   else {
+    return "Computer WON!!!";}
+}
+
+function playRound(playerChoice, computerChoice){
+    const result = checkWinner(playerChoice, computerChoice);
+    if(result == "tie"){
+        return "It's a Tie"
+    }
+    else if(result == "player" ){
+        return `Player WOn!!! ${playerChoice} beats ${computerChoice} !!!`
+    }
+    else{
+        return `Computer Won!!! ${computerChoice} beats ${playerChoice} !!!`
+    }
+}
+
+const computerSelection = computerChoice();
+
+console.log(playRound(playerChoice, computerSelection));
