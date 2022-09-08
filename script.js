4792a4f21f1246bc4c3b4bf1d0e4591dd8077695
TeePee221 <teeepeeee1@gmail.com>
2022-09-08 15:16:23 +0200

    Final result. Everything works.

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 const choices = ["rock", "paper", "scissors"]
 
 
-function playerChoosing() {
-    const playerChoice = prompt("Choose").toLowerCase();
-    console.log(`Player has chosen ${playerChoice}!!!`)
-    return playerChoice
+// function playerChoosing() {
+//     const playerChoice = prompt("Choose Rock Paper or Scissors").toLowerCase();
+//     console.log(`Player has chosen ${playerChoice}!!!`)
+//     return playerChoice
+// }
+
+function playerChoosing(){
+    let validatedInput = false;
+    while(validatedInput == false){
+        const playerChoice = prompt("Choose Rock Paper or Scissors").toLocaleLowerCase();
+        if(playerChoice == null){
+            continue;
+        }
+        if(choices.includes(playerChoice)){
+            validatedInput = true;
+            console.log(`Player has chosen ${playerChoice}!!!`)
+            return playerChoice
+        }
+    }
 }
 
-
 function computerChoice () {
-    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    return computerChoice
+    const computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    return computerSelection
+    
     
 }
 
-function checkWinner (playerChoice, computerChoice){
-    console.log(`Computer has chosen ${computerChoice}!!`)
-    if(playerChoice == computerChoice){
+function checkWinner (playerChoice, computerSelection){
+    if(playerChoice == computerSelection){
         return "tie";
     }
-    else if( (playerChoice == "rock" && computerChoice == "scissors") ||
-            (playerChoice == "scissors" && computerChoice == "paper") ||
-            (playerChoice == "paper" && computerChoice == "rock")
+    else if( (playerChoice == "rock" && computerSelection == "scissors") ||
+            (playerChoice == "scissors" && computerSelection == "paper") ||
+            (playerChoice == "paper" && computerSelection == "rock")
             ) {
                 return "player"
             }
@@ -29,25 +43,47 @@ function checkWinner (playerChoice, computerChoice){
     return "Computer WON!!!";}
 }
 
-function playRound(playerChoice, computerChoice){
-    const result = checkWinner(playerChoice, computerChoice);
+function playRound(playerChoice, computerSelection){
+    const result = checkWinner(playerChoice, computerSelection);
+    console.log(`Computer has chosen ${computerSelection}!!`);
     if(result == "tie"){
         return "It's a Tie"
     }
     else if(result == "player" ){
-        return `Player WOn!!! ${playerChoice} beats ${computerChoice} !!!`
+        return `Player WOn!!! ${playerChoice} beats ${computerSelection} !!!`
     }
     else{
-        return `Computer Won!!! ${computerChoice} beats ${playerChoice} !!!`
+        return `Computer Won!!! ${computerSelection} beats ${playerChoice} !!!`
     }
 }
 
 
 function game (){
+    let playerScore = 0;
+    let computerScore = 0;
     for (let i = 0; i < 5; i++) {
-    const computerSelection = computerChoice();
-    const playerChoice = playerChoosing(); 
-     console.log(playRound(playerChoice,computerSelection));
+        const computerSelection = computerChoice();
+        const playerChoice = playerChoosing(); 
+        console.log(playRound(playerChoice,computerSelection));
+        
+        console.log("------------------")
+         alert("Get ready for next round!!!!")
+        if(checkWinner(playerChoice, computerSelection) == "player"){
+            playerScore++;
+        }
+        else if (checkWinner(playerChoice, computerSelection) == "Computer WON!!!" ){
+            computerScore++;
+        }
+        else if (checkWinner(playerChoice, computerSelection) == "tie"){
+            i--
+        }
+        
+    }
+    if(playerScore > computerScore){
+        console.log(`Player has won ${playerScore} to ${computerScore}!!!!`)
+    }
+    else {
+        console.log(`Computer has won ${computerScore} to ${playerScore}`)
     }
 }
 game()
